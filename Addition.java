9ff9fc662fb9d2751e64bbd3e16a52e6bfe37113
evamsikrishna07 <evamsikrishna07@gmail.com>
2022-05-18 12:27:36 +0530

    created one file

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+	public static void main (string args[])
+	{
+		int num1 num2, res;
+		num1 = 20;
+		num2 = 45;
+		res = num1 + num2;
+		sytstem.out.println ("The result is" +res);
+	}
